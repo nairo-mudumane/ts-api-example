16d6492c0b75ad35284c31f5389f37e10364ab49
nairo-mudumane <nairomudumane@outlook.com>
2022-05-25 15:56:57 +0200

    fixed nodemon

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
-console.log("done.");
-console.log("other");
+import express from "express";
+
+const PORT = process.env.PORT || 3030;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
